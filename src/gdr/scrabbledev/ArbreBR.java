78d7e30507ba4b2c4be4f3ff6b7cfcb802cbc99d
GDRMc <gdrdu86@gmail.com>
2015-12-01 23:17:52 +0100

    TP11 Scrabble - ArbreBR Javadoc

--- src/gdr/scrabbledev/ArbreBR.java
+++ src/gdr/scrabbledev/ArbreBR.java
@@ -4,40 +4,115 @@ import java.util.ArrayList;
 
 public abstract class ArbreBR {
 
+    /**
+     * Renvoie le mot racine
+     * @return Racine actuelle
+     */
     public abstract Mot getRacine();
 
+    /**
+     * Renvoie l'arbre à gauche de la racine
+     * @return Arbre de gauche
+     */
     public abstract ArbreBR getAg();
 
+    /**
+     * Renvoie l'arbre à droite de la racine
+     * @return Arbre de droite
+     */
     public abstract ArbreBR getAd();
 
+    /**
+     * Applique la racine entrée en paramètre sur l'arbre
+     * @param s Mot à insérer
+     */
     public abstract void setRacine(Mot s);
 
+    /**
+     * Applique l'arbre en paramètre sur la racine de gauche
+     * @param Ag Arbre
+     */
     public abstract void setAg(ArbreBR Ag);
 
+    /**
+     * Applique l'arbre en paramètre sur la racine de droite
+     * @param Ad Arbre
+     */
     public abstract void setAd(ArbreBR Ad);
 
+    /**
+     * Insertion dans l'arbre
+     * @param val Valeur à insérer dans l'arbre
+     * @return Etat de l'insertion
+     */
     public abstract ArbreBR insereTo(Mot val);		// nouveau, equivalent � un constructeur
 
+    /**
+     * Recherche d'un mot dans l'arbre
+     * @param val Valeur à rechercher dans l'arbre
+     * @return Etat de l'insertion
+     */
     public abstract boolean rechercherABR(Mot val);
 
+    /**
+     * Renvoie true si l'arbre est vide à la position donnée
+     * @return
+     */
     public abstract boolean estVide();
 
+    /**
+     * Affiche l'arbre à la position donnée
+     */
     public abstract void afficher();
 
+    /**
+     * Renvoie le mot le plus à gauche de la position donnée
+     * @return Mot le plus à gauche
+     */
     public abstract Mot lePlusAGauche();
 
+    /**
+     * Supprime une valeur dans l'arbre
+     * @param val Valeur à supprimer
+     * @return Nouvel arbre ayant subi les modifications
+     */
     public abstract ArbreBR supprimer(Mot val);
 
+    /**
+     * Trouve un mot dans l'arbre à la position actuelle
+     * @param val Mot à rechercher
+     * @return Etat de la recherche
+     */
     public abstract boolean trouver(Mot val);
 
+    /**
+     * Renvoie true si l'arbre actuel à la position donnée est une feuille
+     * @return True si c'est une feuille, sinon False
+     */
     public abstract boolean estFeuille();
 
+    /**
+     * Renvoie le nombre de feuilles de l'arbre à la position actuelle
+     * @return Nombre de feuilles de l'arbre à la position actuelle
+     */
     public abstract int nbFeuilles();
 
+    /**
+     * Retourne le nombre de noeuds de l'arbre à la position donnée
+     * @return Nombre de noeuds
+     */
     public abstract int nbNoeuds();
 
+    /**
+     * Renvoie la hauteur de l'arbre
+     * @return Hauteur de l'arbre
+     */
     public abstract int hauteur();
 
+    /**
+     * Transforme l'arbre actuel à partir de la position donnée en ArrayList fourni en paramètre
+     * @param t ArrayList contenant l'arbre actuel
+     */
     public abstract void arbreBREnTab(ArrayList t);
 
 }
