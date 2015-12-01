f99ebe7c29e9d0a1151f90518c69a691d4359924
GDRMc <gdrdu86@gmail.com>
2015-12-01 23:26:51 +0100

    TP11 Scrabble - Test javadoc + commentaire

--- src/gdr/scrabbledev/Test.java
+++ src/gdr/scrabbledev/Test.java
@@ -1,9 +1,17 @@
 package gdr.scrabbledev;
 
+/**
+ *
+ * @author USER
+ */
 public class Test {
 
+    /**
+     * Méthode de démarrage du programme
+     * @param args
+     */
     public static void main(String[] args) {
-        //titre, largeurfenetre, hauteurfenetre, largeurpanel, hauteurpanel
+        //titre, largeurpanel, hauteurpanel
         Fenetre maFenetre = new Fenetre("Jeu Scrabble", 800, 500);
     }
 
